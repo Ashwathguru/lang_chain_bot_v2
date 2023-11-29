eff02846c77a41e2b3fcff5d69dbcf7744a814ae
Ashwath <ashwathguru.s@bnedloudcloud.com>
2023-11-29 16:28:29 +0530

    app check

--- app.py
+++ app.py
@@ -1,33 +1,56 @@
-from langchain.agents import create_csv_agent
-from langchain.llms import OpenAI
-import os
+from dotenv import load_dotenv
 import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
 
 
 def main():
-    # Load the OpenAI API key from the environment variable
+    load_dotenv()
     OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == OPENAI_API_KEY:
-        print("OPENAI_API_KEY is not set")
-        exit(1)
-    else:
-        print("OPENAI_API_KEY is set")
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF 💬")
+    
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    
+    # extract the text
+    if pdf is not None:
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+      
+      # create embeddings
+      embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
+      
+      # show user input
+      user_question = st.text_input("Ask a question about your PDF:")
+      if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+        
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
+           
+        st.write(response)
+    
 
-    st.set_page_config(page_title="Ask your CSV")
-    st.header("Ask your CSV 📈")
-
-    csv_file = st.file_uploader("Upload a CSV file", type="csv")
-    if csv_file is not None:
-
-        agent = create_csv_agent(
-            OpenAI(temperature=0), csv_file, verbose=True)
-
-        user_question = st.text_input("Ask a question about your CSV: ")
-
-        if user_question is not None and user_question != "":
-            with st.spinner(text="In progress..."):
-                st.write(agent.run(user_question))
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
